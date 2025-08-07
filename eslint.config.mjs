cb12f6c78a38addc60d66a595a2174c420f052eb
mackenziemichellefisher-jpg <mackenziemichellefisher@gmail.com>
2025-08-07 16:20:13 -0700

    Simplify ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,8 +14,7 @@ const eslintConfig = [
   {
     rules: {
       "react/no-unescaped-entities": "off",
-      "no-unused-vars": "warn",
-      "@next/next/no-img-element": "warn"
+      "@next/next/no-img-element": "off"
     }
   }
 ];
